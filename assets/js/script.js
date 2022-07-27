ce0b0407bf9c071fe93bbe10117e86186eff2754
dylanjacob76 <business.dmjacob@gmail.com>
2022-07-27 14:57:26 -0400

    added some comments for future code blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,27 @@ function generatePassword() {
     symbols: "`!@#$%^&*()<>?_+=/"
   }
 
+  // character count
   let charCount = parseInt(prompt("How many characters would you like in your password?"))
   if (charCount > 8 || charCount < 128) {
     console.log(charCount);
   } else {
     alert("You must choose a password length between 8 & 128 charcters.")
   }
+
+  // lowercase letters
+  let charLowercase = confirm("Would you like lowercase letters in your password?");
+  if (charLowercase) {
+    
+  }
+
+  // uppercase letters
+
+  // numbers
+
+  // symbols
+
+  // push to newPassword empty array
 
 }
 
