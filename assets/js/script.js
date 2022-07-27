42b98739a8992195f2d550ba738d17c8d44d3a3e
dylanjacob76 <business.dmjacob@gmail.com>
2022-07-27 14:16:39 -0400

    adding the building blocks of the generatePassword function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,28 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+
+
+function generatePassword() {
+  let newPassword = [];
+  let characterTypes = {
+    letters: "abcdefghijklmnopqrstuvwxyz", // I can use letters.toUpperCase to make captial
+    numbers: "0123456789",
+    symbols: "`!@#$%^&*()<>?_+=/"
+  }
+
+  let charCount = parseInt(prompt("How many characters would you like in your password?"))
+  if (charCount > 8 || charCount < 128) {
+    console.log(charCount);
+  } else {
+    alert("You must choose a password length between 8 & 128 charcters.")
+  }
+
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
