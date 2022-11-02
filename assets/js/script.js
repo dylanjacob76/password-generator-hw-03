58188b2c27217e2f641f1f6e9662a20d75c44bae
dylanjacob76 <business.dmjacob@gmail.com>
2022-11-02 14:42:59 -0400

    added most of the needed JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,5 +10,71 @@ function writePassword() {
 
 }
 
+// Generates password
+function generatePassword() {
+  //create variables for the password and empty array for generated password to go into
+  var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseLetters = lowercaseLetters.toUpperCase();
+  var numbers = "0123456789";
+  var symbols = "!@#$%&*?";
+  var generatedPassword = []
+
+  //password length prompt
+  var length = prompt("How long would you like your password?");
+
+  //verify user entered length meets password criteria
+  if (length < 8 || length > 128) {
+    alert("Please enter a password length between 8 and 128 characters...");
+    // generatePassword();
+  }
+
+
+  //lowercase letter confirm
+  var lowercaseConfirm = confirm("Would you like lowercase letters in your password?");
+
+  if (lowercaseConfirm) {
+    generatedPassword.push(lowercaseLetters);
+  }
+
+
+  //uppercase letter confirm
+  var uppercaseConfirm = confirm("Would you like uppercase letters in your password?");
+
+  if (uppercaseConfirm) {
+    generatedPassword.push(uppercaseLetters);
+  }
+
+
+  //numbers confirm
+  var numbersConfirm = confirm("Would you like numbers in your password?");
+
+  if (numbersConfirm) {
+    generatedPassword.push(numbers);
+  }
+
+
+  //special characters confirm
+  var symbolsConfirm = confirm("Would you like symbols in your password?");
+
+  if (symbolsConfirm) {
+    generatedPassword.push(symbols);
+  }
+
+
+  //all of the symbols are joined within one string
+  generatedPassword = generatedPassword.join("");
+
+  
+  var result = "";
+  
+  for (let i = 0; i < length; i++) {
+    result += generatedPassword[Math.floor(Math.random() * generatedPassword.length)];
+  }
+
+  return result;
+}
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
